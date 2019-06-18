7f1aa84adad6a87767cfbda3c4fe9ab3bc679c28
Matteo Canzari <>
2019-06-18 16:13:49 +0200

    schema and dump data

--- subarray_assign.py
+++ subarray_assign.py
@@ -6,7 +6,7 @@ data = {
                 "receptorIDList": ["0001", "0002"]
             }
         }
-
+#subarray class definition
 class SubArray(object):
     def __init__(self, dish, subarrayID):
         self.dish = dish
@@ -15,6 +15,7 @@ class SubArray(object):
     def __repr__(self):
         return 'SubArray {} is composed of dishIdList {}'.format(self.subarrayID, self.dish)
 
+#dish class definition
 class Dish(object):
     def __init__(self, receptorIDList): 
         self.receptorIDList = receptorIDList
@@ -22,11 +23,30 @@ class Dish(object):
     def __repr__(self): 
         return 'Dish has {} receptorIDList'.format(self.receptorIDList)
 
-
+#create dish and subarray objects
 myDish = Dish(receptorIDList=["0001", "0002"])
 mySubArray = SubArray(1, myDish)
 
 print(myDish)
 
 print(mySubArray)
+
+#definition of Schema
+class SubArraySchema(Schema):
+    dish = fields.String()
+    subarrayID = fields.String()
+
+class DishSchema(Schema): 
+    receptorIDList = fields.String()
+
+#dump data
+subarrayJson = SubArraySchema()
+resultSubArray = subarrayJson.dump(mySubArray)
+
+dishJson = DishSchema()
+result = dishJson.dump(myDish)
+pprint(result)
+
+pprint(resultSubArray)
+
 

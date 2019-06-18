e0fff0a8fc28866409381ad3b66683020f029b43
Matteo Canzari <>
2019-06-18 16:46:14 +0200

    serialization

--- subarray_assign.py
+++ subarray_assign.py
@@ -13,7 +13,7 @@ class SubArray(object):
         self.subarrayID = subarrayID
 
     def __repr__(self):
-        return 'SubArray {} is composed of dishIdList {}'.format(self.subarrayID, self.dish)
+        return 'SubArray {} is composed of receptorIDList {}'.format(self.subarrayID, self.dish)
 
 #dish class definition
 class Dish(object):
@@ -21,31 +21,34 @@ class Dish(object):
         self.receptorIDList = receptorIDList
 
     def __repr__(self): 
-        return 'Dish has {} receptorIDList'.format(self.receptorIDList)
+        idList = ""
+        for receptorId in self.receptorIDList:
+            idList = idList + " " + receptorId
+        return 'Dish has {} receptorIDList'.format(idList)
 
 #create dish and subarray objects
-myDish = Dish(receptorIDList=["0001", "0002"])
-mySubArray = SubArray(1, myDish)
+myDish = Dish(receptorIDList=["0003", "0004"])
+mySubArray = SubArray(2, myDish)
 
 print(myDish)
 
 print(mySubArray)
 
 #definition of Schema
+class DishSchema(Schema): 
+    receptorIDList = fields.List(fields.String()) #https://marshmallow.readthedocs.io/en/3.0/api_reference.html
+
 class SubArraySchema(Schema):
     dish = fields.String()
-    subarrayID = fields.String()
+    subarrayID = fields.Nested(DishSchema) #https://marshmallow.readthedocs.io/en/3.0/nesting.html
 
-class DishSchema(Schema): 
-    receptorIDList = fields.String()
-
+print("serialization Data/JSON")
 #dump data
 subarrayJson = SubArraySchema()
 resultSubArray = subarrayJson.dump(mySubArray)
 
 dishJson = DishSchema()
 result = dishJson.dump(myDish)
-pprint(result)
 
 pprint(resultSubArray)
 

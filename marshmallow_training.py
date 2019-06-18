2585c84ebfea9a4a08066d93768320fc0acfda7a
Matteo Canzari <>
2019-06-18 12:19:53 +0200

    multi Person

--- marshmallow_training.py
+++ marshmallow_training.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields, pprint
+from marshmallow import Schema, fields, pprint, post_load
 
 class Person (object): 
     def __init__(self, name, age, address):
@@ -14,24 +14,22 @@ class PersonSchema(Schema):
     age = fields.Integer()
     address = fields.String()
 
-input_dict = {}
+    @post_load
+    def create_person(self, data):
+        return Person(**data)
 
-input_dict['name'] = input('What is your name? ')
-input_dict['age'] = input('How old are you? ')
-input_dict['address'] = input('What is your address? ')
 
-person = Person(name=input_dict['name'], age=input_dict['age'], address=input_dict['address'])
+#input_dict = {}
 
-#print(person)
+#input_dict['name'] = input('What is your name? ')
+#input_dict['age'] = input('How old are you? ')
+#input_dict['address'] = input('What is your address? ')
 
-schema = PersonSchema()
+#matteo = Person(name=input_dict['name'], age=input_dict['age'], address=input_dict['address'])
 
-result = schema.dump(person)
+matteo = Person(name = 'matteo', address = 'sanzio', age = 34)
+mario = Person(name = 'mario', address = 'roma', age = 19) 
 
-pprint(result)
-
-user_data = {'address': 'roma', 'age': 24, 'name': 'mario'}
-
-mario_schema = PersonSchema()
-result = mario_schema.load(user_data)
+utenti_result = PersonSchema(many=True)
+result = utenti_result.dump([matteo, mario])
 pprint(result)
